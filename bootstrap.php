a76fbf5e5ea6b55601f2ddbfa96da0000e48eae8
Mo Di <modihq@gmail.com>
2019-11-25 17:33:12 +0800

    单元测试的初始化，对取用户 ID 的逻辑进行测试
    去掉对 benbanfa/exclude-composer-assets 的依赖
    
    测试代码使用单独的 .env
    
    在示例项目里启用文件日志

--- bootstrap.php
+++ bootstrap.php
@@ -1,13 +1,6 @@
 <?php
 
-// 自动加载
-
 require dirname(__DIR__).'/vendor/autoload.php';
-require dirname(__DIR__).'/vendor/yiisoft/yii2/Yii.php';
-
-Yii::setAlias('@app', __DIR__);
-
-// 环境变量的处理
 
 if (!isset($_ENV['YII_ENV'])) {
     $dotenv = Dotenv\Dotenv::create(__DIR__);
@@ -16,3 +9,7 @@ if (!isset($_ENV['YII_ENV'])) {
 
 defined('YII_ENV') or define('YII_ENV', $_ENV['YII_ENV'] ?? 'dev');
 defined('YII_DEBUG') or define('YII_DEBUG', $_ENV['YII_DEBUG'] ?? true);
+
+require dirname(__DIR__).'/vendor/yiisoft/yii2/Yii.php';
+
+Yii::setAlias('@app', __DIR__);
